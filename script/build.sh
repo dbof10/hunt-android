25e5139edf2f7d64a8dc53e2d695817351c85880
Daniel Lee <shaymin.android@gmail.com>
2017-11-19 19:43:35 +0700

    fix crash newsletter

--- script/build.sh
+++ script/build.sh
@@ -6,6 +6,8 @@ react-native bundle --platform android --dev false --entry-file live.android.js 
 react-native bundle --platform android --dev false --entry-file jobs.android.js --bundle-output ../assets/react/jobs/index.android.bundle
 react-native bundle --platform android --dev false --entry-file meetups.android.js --bundle-output ../assets/react/meetups/index.android.bundle
 react-native bundle --platform android --dev false --entry-file ask.android.js --bundle-output ../assets/react/ask/index.android.bundle
+react-native bundle --platform android --dev false --entry-file newsletter.android.js --bundle-output ../assets/react/newsletter/index.android.bundle
+
 cd ../../../../
 fi
 
